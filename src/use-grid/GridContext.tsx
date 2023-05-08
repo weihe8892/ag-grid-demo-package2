73e8c9ffcaa2b6e809caec06b538fc65fd2696c9
weihe8892 <wei8892@yahoo.com>
2023-05-08 11:41:32 -0400

    Delete src/use-grid directory

--- src/use-grid/GridContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const GridContext = React.createContext({
-   grid: undefined,
-
-   setGrid: ({gridApi, columnApi}: any) => {
-   },
-
-});
--- src/use-grid/index.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { GridContext } from './GridContext'
-
-
-const useGrid = () => useContext(GridContext)
-
-export default useGrid
